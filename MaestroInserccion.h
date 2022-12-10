9e96d510485aaddcf4e4a605ac4fe52bcd0318a9
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2022-12-09 19:18:53 -0500

    Se crea metodo para guardar jugadores

--- MaestroInserccion.h
+++ MaestroInserccion.h
@@ -53,11 +53,32 @@ void MaestroInserccion::insertarEquipos(){
         cout<<"Ingrese el nombre del entrenador: ";cin>>e.nombreEntrenador;
         cout<<"Ingrese el apellido del entrenador: ";cin>>e.apellidoEntrenador;
         cout<<"Ingrese la nacionalidad del entrenador: ";cin>>e.nacionalidadEntrenador;
-        posEquipos[i]=e.nombre;
+        posEquipos[i]=e.nombre; 
         int x = equipos.insertar(e);
         cout<<"Equipo insertado con exito"<<endl;
     }
     cout<<"\n\nEquipos insertados con exito\n\n";
+}
+
+void MaestroInserccion::insertarJugadores(){
+    cout<<"\n\nAhora insertaremos los jugadores de cada equipo\n\n";
+    for(int i=0;i<8;i++){
+        cout<<"Insertando jugadores del equipo "<<posEquipos[i]<<endl;
+        cout<<"Ingrese el numero de jugadores del equipo: ";cin>>j.numJugadores;
+        for(int j=0;j<j.numJugadores;j++){
+            string nombre;
+            cout<<"Nombre del jugador: ";cin>>nombre;
+            int edad;
+            cout<<"Edad del jugador: ";cin>>edad;
+            int numCamiseta;
+            int cantGoles = 0;
+            string posicion;
+            cout<<"Posicion del jugador: ";cin>>posicion;
+            jugadores.insertar(nombre,edad,numCamiseta,cantGoles,posicion,i);
+            cout<<"Jugador insertado con exito"<<endl;
+        }
+    }
+    cout<<"\n\nJugadores insertados con exito\n\n";
 }
 
 #endif
