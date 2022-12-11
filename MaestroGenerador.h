e397ab9e385bb90e76b57b06f1360659831bf5bc
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2022-12-11 11:59:53 -0500

    Se genera codigo para planear cronograma

--- MaestroGenerador.h
+++ MaestroGenerador.h
@@ -17,22 +17,100 @@ class MaestroGenerador
         //constructor
         MaestroGenerador(){}
         //metodo para recivir una fecha y estadios y generar un cronograma y lo guarda en cronograma
-        void generarCronograma(string dia, string mes, string anio, Estadios estadios);
+        void generarCronograma(Estadios estadios, Equipos equipos);
+        //getter
+        Cronograma getCronograma(){return cronograma;}
         //destructor
         ~MaestroGenerador(){}
 };
 
-void MaestroGenerador::generarCronograma(string dia, string mes, string anio,Estadios estadios){
+void MaestroGenerador::generarCronograma(Estadios estadios, Equipos equipos, int grupoEquipos[]){
     //se deben generar 64 partidos donde 48 son de eliminacion, 8 de octavos, 4 de cuartos, 2 de semifinales, 1 de final y 1 de tercer puesto
     //primeramente se debe generar los partidos de eliminacion
+    int dia = 0;
     bool partidoPorDia = false;
-    for(int i = 0; i<48; i++){
+    for(int i = 0; i<48; i+=4){
         //por dia es un partido
-        if(partidoPorDia){
-            cronograma.insertar(randomEstadio,-1,-1,""+i,"Eliminacion","Aqui debo meter la seccion pero estoy demaciado cansado para hacerlo ._.");
-        }
+        cronograma.insertar(randomEstadio(estadios),i,i+1,""+dia,"Eliminacion",grupoEquipos[i]);
+        cronograma.insertar(randomEstadio(estadios),i+2,i+3,""+dia,"Eliminacion",grupoEquipos[i]);
+        dia++;
+        cronograma.insertar(randomEstadio(estadios),i,i+2,""+dia,"Eliminacion",grupoEquipos[i]);
+        cronograma.insertar(randomEstadio(estadios),i+1,i+3,""+dia,"Eliminacion",grupoEquipos[i]);
+        dia++;
+        cronograma.insertar(randomEstadio(estadios),i,i+3,""+dia,"Eliminacion",grupoEquipos[i]);
+        cronograma.insertar(randomEstadio(estadios),i+1,i+2,""+dia,"Eliminacion",grupoEquipos[i]);
+        dia++;
     }
-    
+    //los -1 a continuacion son para indicar que no hay equipo definido en ese partido
+    //luego se deben generar los partidos de octavos
+    string seccion;
+    for(int i = 48; i<56; i+=2){
+        switch(i){
+            case 48:
+                seccion = "A";
+                break;
+            case 49:
+                seccion = "B";
+                break;
+            case 50:
+                seccion = "C";
+                break;
+            case 51:
+                seccion = "D";
+                break;
+            case 52:
+                seccion = "E";
+                break;
+            case 53:
+                seccion = "F";
+                break;
+            case 54:
+                seccion = "G";
+                break;
+            case 55:
+                seccion = "H";
+                break;
+        }
+        cronograma.insertar(randomEstadio(estadios),-1,-1,""+dia,"Octavos",seccion);
+        dia++;
+    }
+    //luego se deben generar los partidos de cuartos
+    for(int i = 56; i<60; i+=2){
+        switch(i){
+            case 56:
+                seccion = "A";
+                break;
+            case 57:
+                seccion = "B";
+                break;
+            case 58:
+                seccion = "C";
+                break;
+            case 59:
+                seccion = "D";
+                break;
+        }
+        cronograma.insertar(randomEstadio(estadios),-1,-1,""+dia,"Cuartos",seccion);
+        dia++;
+    }
+    //luego se deben generar los partidos de semifinales
+    for(int i = 60; i<62; i+=2){
+        switch(i){
+            case 60:
+                seccion = "A";
+                break;
+            case 61:
+                seccion = "B";
+                break;
+        }
+        cronograma.insertar(randomEstadio(estadios),-1,-1,""+dia,"Semifinales",seccion);
+        dia++;
+    }
+    //luego se debe generar el partido de tercer puesto
+    cronograma.insertar(randomEstadio(estadios),-1,-1,""+dia,"Tercer puesto","A");
+    dia++;
+    //luego se debe generar el partido de final
+    cronograma.insertar(randomEstadio(estadios),-1,-1,""+dia,"Final","A");
     //guardar en cronograma
 }
 //metodo para devolver aleatoriamente un estadio
--- MaestroInserccion.h
+++ MaestroInserccion.h
@@ -16,8 +16,8 @@ class MaestroInserccion
         Estadios estadios;
         Equipos equipos;
         Cronograma cronograma;
-        string posEquipos = new string[32];
-        int grupoEquipos = new int[32];//del 0 al 3 grupo A, del 4 al 7 grupo B, del 8 al 11 grupo C, del 12 al 15 grupo D, del 16 al 19 grupo E, del 20 al 23 grupo F, del 24 al 27 grupo G, del 28 al 31 grupo H
+        string posEquipos[32];
+        int grupoEquipos[32];//del 0 al 3 grupo A, del 4 al 7 grupo B, del 8 al 11 grupo C, del 12 al 15 grupo D, del 16 al 19 grupo E, del 20 al 23 grupo F, del 24 al 27 grupo G, del 28 al 31 grupo H
 	public:
         //constructor
         MaestroInserccion(){
@@ -27,6 +27,13 @@ class MaestroInserccion
         void insertarEstadios();
         void insertarCronograma();
         void insertarDesdeCero();
+        //getters
+        Multilista getJugadores(){return jugadores;}
+        Estadios getEstadios(){return estadios;}
+        Equipos getEquipos(){return equipos;}
+        Cronograma getCronograma(){return cronograma;}
+        string getPosEquipos(){return posEquipos;}
+        int getGrupoEquipos(){return grupoEquipos;}
         //destructores
         ~MaestroInserccion(){
             jugadores.~Multilista();
@@ -47,13 +54,64 @@ void MaestroInserccion::insertarEquipos(){
     cout<<"\n\nEmpezaremos con la inserccion de los 8 equipos participantes en el mundial\n\n";
     Equipo e;
     for(int i=0;i<32;i++){
+        switch(i/4){
+            case 0:
+                cout<<"Ingrese los datos del equipo del grupo A"<<endl;
+                break;
+            case 1:
+                cout<<"Ingrese los datos del equipo del grupo B"<<endl;
+                break;
+            case 2:
+                cout<<"Ingrese los datos del equipo del grupo C"<<endl;
+                break;
+            case 3:
+                cout<<"Ingrese los datos del equipo del grupo D"<<endl;
+                break;
+            case 4:
+                cout<<"Ingrese los datos del equipo del grupo E"<<endl;
+                break;
+            case 5:
+                cout<<"Ingrese los datos del equipo del grupo F"<<endl;
+                break;
+            case 6:
+                cout<<"Ingrese los datos del equipo del grupo G"<<endl;
+                break;
+            case 7:
+                cout<<"Ingrese los datos del equipo del grupo H"<<endl;
+                break;
+        }
         cout<<"Equipo "<<i+1<<" a insertar"<<endl;
         cout<<"Ingrese el nombre del equipo: ";cin>>e.nombre;
         cout<<"Ingrese la confederacion a la que pertenece: ";cin>>e.confederacion;
         cout<<"Ingrese el nombre del entrenador: ";cin>>e.nombreEntrenador;
         cout<<"Ingrese el apellido del entrenador: ";cin>>e.apellidoEntrenador;
         cout<<"Ingrese la nacionalidad del entrenador: ";cin>>e.nacionalidadEntrenador;
-        cout<<"Ingrese el grupo al que pertenece: ";cin>>grupoEquipos[i];
+        switch(i/4){
+            case 0:
+                e.grupo = 'A';
+                break;
+            case 1:
+                e.grupo = 'B';
+                break;
+            case 2:
+                e.grupo = 'C';
+                break;
+            case 3:
+                e.grupo = 'D';
+                break;
+            case 4:
+                e.grupo = 'E';
+                break;
+            case 5:
+                e.grupo = 'F';
+                break;
+            case 6:
+                e.grupo = 'G';
+                break;
+            case 7:
+                e.grupo = 'H';
+                break;
+        }
         posEquipos[i]=e.nombre; 
         int x = equipos.insertar(e);
         cout<<"Equipo insertado con exito"<<endl;
@@ -92,4 +150,6 @@ void MaestroInserccion::insertarEstadios(){
         cout<<"Ingrese la capacidad del estadio: ";cin>>e.capacidad;
     }
 }
+//getters y setters
+
 #endif
